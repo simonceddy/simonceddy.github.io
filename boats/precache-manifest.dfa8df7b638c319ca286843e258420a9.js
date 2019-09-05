166a130623b60f97c5932d2a91902aa9fa14a8fc
Simon Eddy <simon@simoneddy.com.au>
2019-09-05 17:57:55 +1000

    added boats

---
+++ boats/precache-manifest.dfa8df7b638c319ca286843e258420a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87aa32ade0f4c4b72bfe2c1ac521da8b",
+    "url": "/boats/index.html"
+  },
+  {
+    "revision": "a581111f110641ba7c05",
+    "url": "/boats/static/css/main.c70d5c21.chunk.css"
+  },
+  {
+    "revision": "80e87e47f9b8113a397b",
+    "url": "/boats/static/js/2.fed0034e.chunk.js"
+  },
+  {
+    "revision": "a581111f110641ba7c05",
+    "url": "/boats/static/js/main.42c85ab0.chunk.js"
+  },
+  {
+    "revision": "0bbb2f6b07256b83c1e8",
+    "url": "/boats/static/js/runtime~main.7a38b776.js"
+  },
+  {
+    "revision": "498eee562e83efdecae229031bfd83d3",
+    "url": "/boats/static/media/boats1.498eee56.svg"
+  }
+]);
