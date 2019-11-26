ae307a375e5ebdf937b382de29ed1ef922bbdaac
Simon Eddy <simon@simoneddy.com.au>
2019-11-26 23:22:11 +1100

    added songmath

---
+++ songmath/precache-manifest.a5a2ebe4f2406a84f6977f58b66e196f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecc7fcd6993c9fd42a3d7ec87abf98a",
+    "url": "/songmath/index.html"
+  },
+  {
+    "revision": "3c5ca332c34246f5460c",
+    "url": "/songmath/static/css/main.dd1e8c6b.chunk.css"
+  },
+  {
+    "revision": "35b64a9f9c8b3c9bb1a0",
+    "url": "/songmath/static/js/2.a67bf317.chunk.js"
+  },
+  {
+    "revision": "3c5ca332c34246f5460c",
+    "url": "/songmath/static/js/main.734d57af.chunk.js"
+  },
+  {
+    "revision": "14b3dcdc7e0172f91bfa",
+    "url": "/songmath/static/js/runtime-main.0264e14c.js"
+  }
+]);
